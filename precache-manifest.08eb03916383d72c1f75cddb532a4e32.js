363acaf5c2c9b71e76b39d37abcbee0e4fc3be7e
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2022-05-25 21:04:58 -0300

    Initial commit

---
+++ precache-manifest.08eb03916383d72c1f75cddb532a4e32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61283d6e31e8702a0742",
+    "url": "./static/css/main.d1e605de.chunk.css"
+  },
+  {
+    "revision": "61283d6e31e8702a0742",
+    "url": "./static/js/main.83682c4e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0ca4cbcca696215cc197",
+    "url": "./static/js/2.5f3ae66a.chunk.js"
+  },
+  {
+    "revision": "766d706f1963a1c5fb703734b6a3b811",
+    "url": "./index.html"
+  }
+];
